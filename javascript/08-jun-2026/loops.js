64b65c1bcb49b34b14f49c641a9c990fca74f846
Brammanayagan S <sakthibrammanayagan@gmail.com>
2026-06-08 13:49:35 +0530

    loops-done

--- javascript/08-jun-2026/loops.js
+++ javascript/08-jun-2026/loops.js
@@ -1,3 +1,21 @@
 //object value loop - print using for in
 
+const data1 = {
+  name: "Bramma",
+  age: 24,
+  city: "Chennai",
+};
+
+for (let ans in data1) {
+  console.log(data1[ans]);
+}
+
 //array value map/for loop/ for each
+
+let arr = ["Bramma", "Vishawa", "Raj Bharath"];
+
+const list = arr.map((items) => console.log("map", items));
+
+for (let i = 0; i < arr.length; i++) {
+  console.log("for", arr[i]);
+}
