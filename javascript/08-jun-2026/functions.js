0c26faae0ecc81cf1ca72bfecd17c551efa2b5b2
Brammanayagan S <sakthibrammanayagan@gmail.com>
2026-06-08 13:24:56 +0530

    inital commit gor task-git

---
+++ javascript/08-jun-2026/functions.js
@@ -0,0 +1,5 @@
+//function with argument and paameters only Arrow function
+
+//callback function any method
+
+//closure program
---
+++ javascript/08-jun-2026/loops.js
@@ -0,0 +1,3 @@
+//object value loop - print using for in
+
+//array value map/for loop/ for each
